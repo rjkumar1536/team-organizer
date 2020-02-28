ce43340cfb4ec7357254f6384b0ff59d90c81ecf
I328696 <srikrishna.charan.kalavacharla@sap.com>
2020-02-28 16:23:57 +0530

    test changes

--- server/Schema/CommSchema.js
+++ server/Schema/CommSchema.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+// schema for communication
 const schema = new mongoose.Schema({
-  user_id : String,
-	message_id : String,
-	message : String,
-	status : Number,
-  created_on : Date,
+  user_id: String,
+  message_id: String,
+  message: String,
+  status: Number,
+  created_on: Date,
   creation_time: Date
-})
+});
 
 module.exports = schema;
